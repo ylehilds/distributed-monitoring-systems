5b1e19376adbb4298f33e0d1f03becfd1c7524ec
Tyler Etchart <tyler.etchart@gmail.com>
2018-10-11 08:45:06 -0600

    Added cockroachdb to repo

---
+++ scraper/data_checker.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import time
+import requests
+import logging
+from datetime import datetime
+from pymongo import MongoClient
+from data_processor import DataProcessor
+
+
+def main():
+    # set up mongo client
+    client = MongoClient("192.168.0.1", port=30001)
+    db = client.business
+    c = db.prometheus.find({}).count()
+    print("In prometheus: {}".format(c))
+    print("In 35: {}".format(db.prometheus["35"].find({}).count()))
+    print("In 75: {}".format(db.prometheus["75"].find({}).count()))
+    db.prometheus["35"].drop()
+
+if __name__ == "__main__":
+    main()
