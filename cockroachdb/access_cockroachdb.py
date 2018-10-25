09cff4a910e50105c88a150a5b77c973987ebed3
Tyler Etchart <tyler.etchart@gmail.com>
2018-10-25 09:03:22 -0600

    added a test script for accessing data

--- cockroachdb/access_cockroachdb.py
+++ cockroachdb/access_cockroachdb.py
@@ -1,35 +1,39 @@
-from __future__ import print_function
+#!/usr/bin/env python
+
+import requests
+import click
+
 from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import sqlalchemy.types as sqltypes
-import uuid
 
-Base = declarative_base()
 
-class Metrics(Base):
-    __tablename__ = 'metrics'
-    id = Column(sqltypes.VARCHAR, primary_key=True)
-    ip = Column(sqltypes.VARCHAR)
-    metric_name = Column(sqltypes.VARCHAR)
-    labels = Column(sqltypes.JSON)
-    metric_value = Column(sqltypes.FLOAT)
+@click.command()
+@click.option("--ip", help="ip for connecting to the database")
+@click.option("--port", help="port for database"
+def main(ip):
+    # set up cockroachdb client
+    Base = declarative_base()
 
-# Create an engine to communicate` with the database. The "cockroachdb://" prefix
-# for the engine URL indicates that we are connecting to CockroachDB.
-engine = create_engine('cockroachdb://root@192.168.99.100:31683/prometheus')
-Session = sessionmaker(bind=engine)
+    class Metrics(Base):
+        __tablename__ = 'metrics'
+        id = Column(sqltypes.VARCHAR, primary_key=True)
+        ip = Column(sqltypes.VARCHAR)
+        metric_name = Column(sqltypes.VARCHAR)
+        labels = Column(sqltypes.JSON)
+        metric_value = Column(sqltypes.FLOAT)
 
-# Automatically create the "accounts" table based on the Account class.
-Base.metadata.create_all(engine)
+    engine = create_engine(
+        'cockroachdb://root@{}:{}/system'.format(ip, port))
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-# Insert two rows into the "accounts" table.
-session = Session()
-session.add_all([
-    Metrics(id=str(uuid.uuid4()), ip="127.0.0.1", metric_name="wmi", labels="{\"id\": 3}", metric_value=3.4)
-])
-session.commit()
+    result = engine.execute("select * from metrics limit 10")
+    for metric in result:
+        print(metric.ip, metric.metric_name, metric.labels,
+              metric.metric_value)
 
-# Print out the balances.
-for metric in session.query(Metrics):
-    print(metric.ip, metric.metric_name, metric.labels, metric.metric_value)
+
+if __name__ == "__main__":
+    main()
