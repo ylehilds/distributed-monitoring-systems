5d339225ae84f1b1f2be1a2b70f32a181cb6a945
Lehi <lehi_alcantara@byu.edu>
2018-11-08 14:40:34 -0700

    removed getIP method as we are just passing in when docker run command is initalized

--- monitoring/server_checker.py
+++ monitoring/server_checker.py
@@ -13,18 +13,6 @@ import os
 # list of servers to check with the following items in the
 # definitions per-server: ('hostname', 'ssl or plain', portnumber)
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-
 host_ip = os.environ['HOST_IP']
 print(host_ip)
 
